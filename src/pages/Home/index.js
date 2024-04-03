ce70a2c8573965b7f973958e8b0c144350b9982b
Maycon-jeferson <jefersonmaycon3@gmail.com>
2024-04-03 09:49:07 -0300

    adicionado imagen de perfil e descricao

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,37 @@
 import { Header } from "../../components/Header";
 
+
+import './styles.css'
+
 function App() {
   return (
     <div className="App">
+
       <Header />
+      <div className="conteudo">
+
+        <div className="info">
+
+          <div>
+            <input name="usuario" placeholder="@username"/>
+            <button>buscar</button>
+          </div>
+
+            <div className="perfil">
+              <img src="https://avatars.githubusercontent.com/u/148920537?v=4" className="profile" alt=""/>
+              <div>
+                <h3>Maycon Jeferson</h3>
+                <span>@maycon</span>
+                <p>descricao</p>
+              </div>
+          </div>
+
+          <hr />
+          
+        </div>
+
+      </div>
+
     </div>
   );
 }
